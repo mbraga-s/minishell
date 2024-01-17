f3d3194ed2ebe47cf42b01b873f4f9a98aed7e22
mbraga-s <mbraga-s@students.42lisboa.com>
2024-01-17 16:16:33 +0000

    Working lexer, split not up to norm

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,36 +6,47 @@
 /*   By: mbraga-s <mbraga-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 19:34:57 by mbraga-s          #+#    #+#             */
-/*   Updated: 2024/01/12 16:28:28 by mbraga-s         ###   ########.fr       */
+/*   Updated: 2024/01/17 16:11:55 by mbraga-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int	inv_comma(char const *ptr, int i, char c)
+{
+	int	count;
+
+	count = 0;
+	while (ptr[++i])
+	{
+		if (ptr[i] == c)
+			break ;
+		count++;
+	}
+	return (count);
+}
+
 int	ft_wcount(char const *s, char c)
 {
 	int	i;
 	int	sep;
-	int	flag;
 	int	count;
 
 	i = 0;
 	sep = 1;
-	flag = 0;
 	count = 0;
-	while (s[i++])
+	while (s[i])
 	{
-		if (s[i] == 39 && flag == 0)
-			flag = 1;
-		else if (s[i] == 39 && flag == 1)
-			flag = 0;
-		else if (s[i] == c && flag == 0)
+		if (s[i] == 34 || s[i] == 39)
+			i = i + 1 + inv_comma(s, i, s[i]);
+		if (s[i] == c)
 			sep = 1;
-		else if (sep == 1 && s[i] != c && flag == 0)
+		if (sep == 1 && s[i] != c)
 		{
 			count++;
 			sep = 0;
 		}
+		i++;
 	}
 	return (count);
 }
@@ -45,7 +56,7 @@ int	ft_wlen(char const *s, char c, int i)
 	int	len;
 
 	len = 0;
-	while (s[i] != c && s[i])
+	while (s[i] && s[i] != c)
 	{
 		i++;
 		len++;
@@ -53,7 +64,7 @@ int	ft_wlen(char const *s, char c, int i)
 	return (len);
 }
 
-/* char	**ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
 	char	**ptr;
 	int		i;
@@ -69,20 +80,42 @@ int	ft_wlen(char const *s, char c, int i)
 	{
 		while (s[i] == c)
 			i++;
-		j = ft_wlen(s, c, i);
-		ptr[k] = ft_substr(s, i, j);
+		if (s[i] == 34 || s[i] == 39)
+		{
+			j = inv_comma(s, i, s[i]);
+			i++;
+			ptr[k] = ft_substr(s, i, j);
+			i = i + j + 1;
+		}
+		else
+		{
+			j = ft_wlen(s, c, i);
+			ptr[k] = ft_substr(s, i, j);
+			i = i + j;
+		}
 		k++;
-		i = i + j;
 	}
 	return (ptr);
-} */
-
-int	main(void)
-{
-	char *ptr = "Hello 'son of a bitch' im in.";
-	int	i;
-
-	i = ft_wcount(ptr, ' ');
-	printf("%d\n", i);
-	return (0);
 }
+
+/* int	main(int argc, char **argv)
+{
+	int		i;
+	char	**ptr;
+
+	i = 0;
+	if (argc == 2)
+	{
+		ptr = ft_split(argv[1], ' ');
+		while (ptr[i])
+		{
+			printf("%s\n", ptr[i]);
+			free (ptr[i]);
+			i++;
+		}
+		free (ptr);
+ 		//i = ft_wcount(argv[1], ' ');
+		//printf("words = %d\n", i);
+	}
+	return (0);
+} */
