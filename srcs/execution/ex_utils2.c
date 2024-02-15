7b43cf27be1642390878b9a3f9acf3d7afee0998
mbraga-s <miguel.soares.correia@outlook.com>
2024-02-15 18:19:55 +0000

    Almost everything up to norm - working on append mode

--- srcs/execution/ex_utils2.c
+++ srcs/execution/ex_utils2.c
@@ -6,7 +6,7 @@
 /*   By: mbraga-s <mbraga-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 15:41:51 by mbraga-s          #+#    #+#             */
-/*   Updated: 2024/02/15 15:50:36 by mbraga-s         ###   ########.fr       */
+/*   Updated: 2024/02/15 18:15:18 by mbraga-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,54 @@
 //If there is, opens them and dups the file descriptor to stdin or stdout
 //Works for multiple infiles and outfiles
 
+int	infile_check(int dups[2], t_data *data);
+
+int	outfile_check(int dups[2], t_data *data);
+
 int	file_check(int dups[2], t_data *data)
 {
-	int	fd[2];
+	if (infile_check(dups, data) && outfile_check(dups, data))
+		return (1);
+	return (0);
+}
+
+int	infile_check(int dups[2], t_data *data)
+{
+	int	fd;
 	int	i;
 
 	i = 0;
 	while (data->infile && data->infile[i])
 	{
-		fd[0] = open(data->infile[i], O_RDONLY);
-		if (fd[0] < 0)
+		fd = open(data->infile[i], O_RDONLY);
+		if (fd < 0)
 		{
 			perror(data->infile[i]);
 			return (0);
 		}
-		dups[0] = dupcheck(fd[0], dups[0]);
-		close(fd[0]);
+		dups[0] = dupcheck(fd, dups[0]);
+		close(fd);
 		i++;
 	}
+	return (1);
+}
+
+int	outfile_check(int dups[2], t_data *data)
+{
+	int	fd;
+	int	i;
+
 	i = 0;
 	while (data->outfile && data->outfile[i])
 	{
-		fd[1] = open(data->outfile[i], O_RDWR | O_CREAT | O_TRUNC, 0644);
-		if (fd[1] < 0)
+		fd = open(data->outfile[i], O_RDWR | O_CREAT | O_TRUNC, 0644);
+		if (fd < 0)
 		{
 			perror(data->outfile[i]);
 			return (0);
 		}
-		dups[1] = dupcheck(fd[1], dups[1]);
-		close(fd[1]);
+		dups[1] = dupcheck(fd, dups[1]);
+		close(fd);
 		i++;
 	}
 	return (1);
