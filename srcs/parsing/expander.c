a95c17d7eadb0fc902aed1690fdc8256e69cb10e
mbraga-s <miguel.soares.correia@outlook.com>
2024-02-22 15:15:08 +0000

    Fixed last error with expander

--- srcs/parsing/expander.c
+++ srcs/parsing/expander.c
@@ -6,7 +6,7 @@
 /*   By: mbraga-s <mbraga-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 12:31:37 by mbraga-s          #+#    #+#             */
-/*   Updated: 2024/02/22 14:06:32 by mbraga-s         ###   ########.fr       */
+/*   Updated: 2024/02/22 15:12:19 by mbraga-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,20 @@
 char	*rem_quotes(char *str);
 
 char	*expand(char *str, char **envp);
+
+void	check_args(char **args, int pos)
+{
+	if (args[pos] && args[pos][0] == '\0')
+	{
+		free(args[pos]);
+		while (args[pos + 1])
+		{
+			args[pos] = args[pos + 1];
+			pos++;
+		}
+		args[pos] = NULL;
+	}
+}
 
 // Checks if there are quoted or expandable strings
 
@@ -31,11 +45,15 @@ void	expander(t_data *current, char **envp)
 			{
 				current->args[j] = rem_quotes(current->args[j]);
 				current->args[j] = expand(current->args[j], envp);
+				check_args(current->args, j);
 			}
 			else if (current->args[j][0] == 39)
 				current->args[j] = rem_quotes(current->args[j]);
 			else
+			{
 				current->args[j] = expand(current->args[j], envp);
+				check_args(current->args, j);
+			}
 			j++;
 		}
 		current = current->next;
