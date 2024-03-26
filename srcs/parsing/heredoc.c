7ea2c25ef8e7143239384b7a61b6567ef185a48f
mbraga-s <miguel.soares.correia@outlook.com>
2024-03-26 17:44:52 +0000

    FINISHED?

--- srcs/parsing/heredoc.c
+++ srcs/parsing/heredoc.c
@@ -6,11 +6,17 @@
 /*   By: mbraga-s <mbraga-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:07:27 by mbraga-s          #+#    #+#             */
-/*   Updated: 2024/03/26 17:30:22 by mbraga-s         ###   ########.fr       */
+/*   Updated: 2024/03/26 17:43:20 by mbraga-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+void	input_error(void)
+{
+	ft_putstr(2, "warning: here-document delimited by EOF.\n");
+	exit(0);
+}
 
 //Opens a pipe as a way to write to a process and writes the input to it.
 //Returns the read end of the pipe.
@@ -24,16 +30,17 @@ int	ft_heredoc(char *str)
 		perror(NULL);
 		return (0);
 	}
-	input = NULL;
-	signal(SIGQUIT, sigquithandler);
 	signal(SIGINT, sigheredochandler);
 	input = readline("> ");
+	if (!input)
+		input_error();
 	while (ft_strncmp(input, str, (ft_strlen(str) + 1)))
 	{
 		free(input);
 		input = readline("> ");
-		input = expand(input, msdata()->envp);
-		input = rem_quotes(input);
+		if (!input)
+			input_error();
+		input = rem_quotes(expand(input, msdata()->envp));
 		write(fd[1], input, ft_strlen(input));
 	}
 	write(fd[1], "\n", 1);
